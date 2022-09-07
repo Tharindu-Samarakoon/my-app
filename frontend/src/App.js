da21e51c51cf87438d9ef74f3a3ec49690fa0fd6
Tharindu Samarakoon <tharusamara@gmail.com>
2022-09-08 02:40:20 +0530

    Made changes to app.js and server.js to run

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,8 +9,8 @@ import Home from "./components/Home";
 import ViewAppointments from './components/ViewAppointments';
 import Appointments from './components/Appointments';
 import AddAppointments from './components/AddAppointments';
-import ComponentToPrint, { Example } from './components/AppointmentsReport';
-import UpdateAppointments from './components/UpdateAppointments'
+// import ComponentToPrint, { Example } from './components/AppointmentsReport';
+// import UpdateAppointments from './components/UpdateAppointments'
 import './appointment.css'
 
 
@@ -19,17 +19,17 @@ import './appointment.css'
 //Lab Tests
 import AddLab_c from './components/AddLab_c';
 import ViewLabs_c from './components/ViewLabs_c';
-import EditLab_c from './components/EditLab_c';
+// import EditLab_c from './components/EditLab_c';
 import labDashboard from './components/LabDashboard';
-import LabReport_c from './components/LabReport_c';
+// import LabReport_c from './components/LabReport_c';
 import './lab_c.css';
 
 //Payment
 import paymentView_h from "./components/paymentView_h";
 import paymentAdd_h from "./components/paymetAdd_h";
 import payment_h from "./components/payment_h";
-import ExamplePayment from "./components/paymentReport";
-import updatePayment from "./components/updatePayment";
+// import ExamplePayment from "./components/paymentReport";
+// import updatePayment from "./components/updatePayment";
 import './payment_h.css'
 
 
@@ -57,21 +57,21 @@ export default class App extends Component{
         <Route path = "/viewAppointments" exactc component={ViewAppointments}></Route>  
         <Route path = "/Appointments" exactc component={Appointments}></Route>
         <Route path = "/addAppointments" exactc component={AddAppointments}></Route> 
-        <Route path= "/updateAppointments/:id" exact component={UpdateAppointments}></Route>
-        <Route path="/appointmentReport" exact component={Example }/>
+        {/* <Route path= "/updateAppointments/:id" exact component={UpdateAppointments}></Route>
+        <Route path="/appointmentReport" exact component={Example }/> */}
            {/*lab test */}
       <Route path="/labs" exact component={ViewLabs_c}/>
       <Route path="/save" exact component={AddLab_c}/>
-      <Route path="/editLab/:id" exact component={EditLab_c}/>
+      {/* <Route path="/editLab/:id" exact component={EditLab_c}/> */}
       <Route path="/labDashboard" exact component={labDashboard}/>
-      <Route path="/PrintLabReport" exact component={LabReport_c}/>      
+      {/* <Route path="/PrintLabReport" exact component={LabReport_c}/>       */}
 
               {/* Payment Routes */}
         <Route path="/viewpayment" exact component ={paymentView_h}></Route>
         <Route path="/payadd" exact component ={paymentAdd_h}></Route>
         <Route path="/payment" exact component ={payment_h}></Route>
-        <Route path="/payUpdate/:id" exact component={updatePayment}></Route>
-        <Route path="/paymentreport" exact component ={ExamplePayment}></Route>
+        {/* <Route path="/payUpdate/:id" exact component={updatePayment}></Route> */}
+        {/* <Route path="/paymentreport" exact component ={ExamplePayment}></Route> */}
         {/*Staff*/}
         <Route path="/add" exactc component={AddStaff}/>
         <Route path="/view" exact component={ViewStaff}/>
