b389aef1e1c482de71dd2173be889913e72dfbac
Tharindu Samarakoon <tharusamara@gmail.com>
2022-09-09 04:17:20 +0530

    changes to footer and staff dashboard

--- frontend/src/components/StaffDashboard_t.js
+++ frontend/src/components/StaffDashboard_t.js
@@ -11,27 +11,58 @@ function StaffDash(props){
             <h2 className="text-center"><b>Staff Management</b></h2>
             <br></br><br></br><br></br>
 
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4" style={{marginLeft:200}}>
+            <div class="row justify-content-evenly gx-5">
             <br></br>
             
-            <div className=" col "  style={{marginLeft:1}}>
+            {/* <div className=" col "  style={{marginLeft:1}}>
             <Link to="/add" className="addstaff">
                 <img src="/images/add staff.png" width="240" height="240" alt="" /><br></br><br></br>
                 <button className="btn btn-info" style={{marginLeft:17}}><h6><b> Add New Staff Member</b></h6></button>
             </Link>
+            </div> */}
+            <div className="col" style={{marginLeft: 20, marginRight: 20}}>
+            <Link to="/add" className="addstaff" style={{textDecoration: 'none', color: 'black'}}>
+                <div class="card text-center">
+                <img src="https://images.pexels.com/photos/4021779/pexels-photo-4021779.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width={450} class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <p class="card-text"><h6><b> Add New Staff Member</b></h6></p>
+                </div>
+            </div>
+            </Link>
             </div>
 
-            <div className="col " style={{marginLeft:50}} >
+            {/* <div className="col " style={{marginLeft:50}} >
             <Link to="/view" className="viewstaff">
                 <img src="/images/view new staff.png" width="240" height="240" alt="" /><br></br><br></br>
                 <button className="btn btn-info" style={{marginLeft:70}}><h6><b>Staff List</b></h6></button>
             </Link>
+            </div> */}
+            <div className="col" style={{marginLeft: 20, marginRight: 20}}>
+            <Link to="/add" className="addstaff" style={{textDecoration: 'none', color: 'black'}}>
+                <div class="card text-center">
+                <img src="https://images.pexels.com/photos/1170979/pexels-photo-1170979.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width={450} class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <p class="card-text"><h6><b>Staff List</b></h6></p>
+                </div>
+            </div>
+            </Link>
             </div>
 
-            <div className="col "  style={{marginLeft:50}}>
+            {/* <div className="col "  style={{marginLeft:50}}>
             <Link to="/report" className="staffreport">
                 <img src="/images/report.png" width="240" height="240" alt="" /><br></br><br></br>
                 <button className="btn btn-info" style={{marginLeft:70}}><h6><b> Staff Report</b></h6></button>
+            </Link>
+            </div> */}
+
+            <div className="col" style={{marginLeft: 20, marginRight: 20}}>
+            <Link to="/add" className="addstaff" style={{textDecoration: 'none', color: 'black'}}>
+                <div class="card text-center">
+                <img src="https://images.pexels.com/photos/590016/pexels-photo-590016.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" width={450} class="card-img-top" alt="..."/>
+                <div class="card-body">
+                    <p class="card-text"><h6><b>Staff Report</b></h6></p>
+                </div>
+            </div>
             </Link>
             </div>
 
